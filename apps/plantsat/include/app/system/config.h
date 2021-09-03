c737acab4a489a8d084cd6f92ae49c418667bc93
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2021-09-03 18:53:23 -0400

    Add missing variables

--- apps/plantsat/include/app/system/config.h
+++ apps/plantsat/include/app/system/config.h
@@ -2,12 +2,16 @@
 #define	SUCHAI_APP_CONFIG_H
 
 #define SCH_TNC_ADDRESS        9  ///< Nanocom GS100 address
+#define SCH_TRX_ADDRESS        5  ///< Nanocom GS100 address
 #define SCH_TX_FREQ            425000000  ///< Nanocom TX freq in Hz
 #define SCH_TX_PWR             0  ///< Nanocom TX power (0: 25dBm, 1: 27dBm, 2: 28dBm, 3: 30dBm)
 #define SCH_TX_BAUD            4800  ///< Nanocom TX Baudrate bps (4800, 9600, 19200)
 #define SCH_TX_BCN_PERIOD      600  ///< Number of seconds between trx beacon packets
 #define SCH_OBC_BCN_OFFSET     600  ///< Number of seconds between obc beacon packets
+#define SCH_WDT_PERIOD          120
 
-#define SCH_WDT_PERIOD          30
+#define SCH_HK_ENABLED 1
+#define SCH_SEN_ENABLED 1
+#define SCH_ADCS_ENABLED 0
 
 #endif //SUCHAI_APP_CONFIG_H
