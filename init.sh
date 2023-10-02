d52cbc28cbe4b72d8df0b8df55da8fe66ab9d6f3
Carlos Gonzalez <contacto.carlosgonzalez@gmail.com>
2023-10-02 23:47:16 +0200

    Remove satellite driver dependency

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 echo "Downloading SUCHAI Flight Software"
 [ ! -d suchai-flight-software ] && git clone -b feature/framework https://gitlab.com/spel-uchile/suchai-flight-software.git
 
-echo "Downloading BUS drivers..."
-cd apps/groundstation/src/drivers || exit
-[ ! -d suchai-drivers-bus ] && git clone https://gitlab.com/spel-uchile/suchai-drivers-bus suchai-drivers-bus
-cd - || exit
+#echo "Downloading BUS drivers..."
+#cd apps/groundstation/src/drivers || exit
+#[ ! -d suchai-drivers-bus ] && git clone https://gitlab.com/spel-uchile/suchai-drivers-bus suchai-drivers-bus
+#cd - || exit
